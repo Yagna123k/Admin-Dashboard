2fc0c4c3dbde472321cb60ba0a8f9fde9b442403
Yagna123k <yagna82kb@gmail.com>
2024-03-17 13:02:58 +0530

    Server Setup Completed

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Super Admin Dashboard API');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
